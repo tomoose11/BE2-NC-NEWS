3f006af0de8bb7231f7d5755f30bb33a0b633ba3
tomoose11 <tomwall_84@hotmail.co.uk>
2018-11-23 15:16:40 +0000

    heroku1

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,14 @@
-const knex = require('knex');
-const config = require('../knexfile');
-
 const ENV = process.env.NODE_ENV || 'development';
+const config = ENV === 'production' ? { client: 'pg', connection: process.env.DB_URL } : require('../knexfile')[ENV];
 
-const db = knex(config[ENV]);
+module.exports = require('knex')(config);
 
-module.exports = db;
+
+// const knex = require('knex');
+// const config = require('../knexfile');
+
+// const ENV = process.env.NODE_ENV || 'development';
+
+// const db = knex(config[ENV]);
+
+// module.exports = db;
