f804ce2923261c786f46f9e0f49caf72b505cebd
tomoose11 <tomwall_84@hotmail.co.uk>
2018-11-23 15:38:03 +0000

    change db-url

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const ENV = process.env.NODE_ENV || 'development';
-const config = ENV === 'production' ? { client: 'pg', connection: process.env.DB_URL } : require('../knexfile')[ENV];
+const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile')[ENV];
 
 module.exports = require('knex')(config);
 
