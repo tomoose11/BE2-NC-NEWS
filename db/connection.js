6851b0617cae50df838061c93860fc9acda205ab
tomoose11 <tomwall_84@hotmail.co.uk>
2022-03-20 22:58:43 +0000

    clean up

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,5 @@
 const ENV = process.env.NODE_ENV || 'development';
-const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL_2, ssl: { rejectUnauthorized: false } } : require('../knexfile')[ENV];
+const config = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } } : require('../knexfile')[ENV];
 
 module.exports = require('knex')(config);
 
