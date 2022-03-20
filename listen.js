469a7b44ee0e657235b1084ff0e482332f72db22
tomoose11 <tomwall_84@hotmail.co.uk>
2022-03-20 21:08:36 +0000

    clean up

--- listen.js
+++ listen.js
@@ -5,5 +5,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 9090;
 
 app.listen(PORT, () => {
-  console.log(`listening on port${PORT}`);
+  console.log(`listeningn on port${PORT}`);
 });
